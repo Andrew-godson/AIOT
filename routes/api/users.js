ac3bafe9d36e2bd684518770b6edf548722dfb64
unknown <andrewjebrajgodson.18ee@kct.ac.in>
2021-03-10 15:28:40 +0530

    with user verification

---
+++ routes/api/users.js
@@ -0,0 +1,53 @@
+const express=require("express");
+const router=express.Router();
+const {check,validationResult}=require("express-validator");
+const User=require("../../models/Users");
+const grav=require('gravatar');
+const bycrpt=require('bcryptjs');
+
+router.post('/',[
+check("name","Enter Your Name").not().isEmpty(),
+check("email","Enter a vaild email ID").isEmail(),
+check("password","Password is short,Minimum of 6 Characters").isLength({min:6})
+
+],
+async(req,res)=>{
+    const{name,email,password}=req.body;
+    const errors=validationResult(req);
+    if (!errors.isEmpty())
+    {
+        return res.status(400).json({errors:errors.array()})
+    }
+    try{
+        //CHECK IF USER EXSISTS:
+        let user= await User.findOne({email});
+        if(user){
+            return res.status(404).json({msg:"User EXSIST'S"})
+        }
+       // GRAVATAR
+        const avatar=grav.url(email,{
+            s:"200",
+            r:'pg',
+            d:'mm'
+
+        });
+        // ECRYPTING THE PASSWORD
+        const newuser=new User({
+            name,email,avatar,password
+        });
+        const salt=await bycrpt.genSalt(10);
+        newuser.password=await bycrpt.hash(password,salt);
+        await newuser.save();
+
+
+        res.send('User Registered!!!');
+    }
+    catch(err){
+console.error(err);
+res.status(500).send("Server Not Responding");
+    }
+    
+    
+});
+
+module.exports=router;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const { response, request } = require('express');
-const express = require('express');
+
+const express =require('express');
 const app = express();
-
-app.get('/',(req,res)=>res.send("APP RUNNING!!!"));
-
-
-
+const connectDB=require('./config/db')
+app.get('/',(req,res)=>res.send("APP RUNNING SUCCESSFULLY!!!"));
+connectDB();
+app.use(express.json({extended:false}));
+app.use("/api/users",require("./routes/api/users"));
+app.use("/api/auth",require("./routes/api/auth"));
+app.use("/api/profile",require("./routes/api/profile"));
+app.use("/api/post",require("./routes/api/post"));
 
 
 const PORT=process.env.PORT || 1000;
